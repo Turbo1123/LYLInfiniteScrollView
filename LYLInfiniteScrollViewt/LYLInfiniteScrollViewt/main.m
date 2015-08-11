7c0a9d5ea2bdfdd31188e7908dac883d5aa6aac0
liyunlong1123 <liyunlong3189@163.com>
2015-08-11 09:28:18 +0800

    init code

---
+++ LYLInfiniteScrollViewt/LYLInfiniteScrollViewt/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LYLInfiniteScrollViewt
+//
+//  Created by 李云龙 on 15/8/11.
+//  Copyright (c) 2015年 hihilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
